d4ff23cf4ac8cfc7e418e3d7ca9c4fb8e0f70154
Hayley Son <hayleyson92@gmail.com>
2023-01-02 22:51:01 +0900

    [10952] cleaned up imports

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,21 +1,20 @@
 #include <iostream>
-#include <cstdio>
 using namespace std;
 
-int main(){
-    
- int a, b;
+int main()
+{
 
- while (true){
-     
-    cin >> a >> b;
-    if ((a == 0) && (b == 0))
-        break;
-    
-    cout << a + b << '\n';
+    int a, b;
 
- } 
+    while (true)
+    {
 
- 
- return 0;   
+        cin >> a >> b;
+        if ((a == 0) && (b == 0))
+            break;
+
+        cout << a + b << '\n';
+    }
+
+    return 0;
 }
