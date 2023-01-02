10642673203abec2514236af056671d8d4c260f7
Hayley Son <hayleyson92@gmail.com>
2023-01-02 21:43:47 +0900

    [Bronze V] Title: A+B - 8, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+ int n;
+ cin >> n;
+ 
+ int a[n], b[n];
+ 
+ for (int i = 0; i < n; i++){
+    cin >> a[i] >> b[i];
+ }
+ 
+ for (int i = 0; i < n; i++){
+    
+    cout << "Case #" << (i+1) << ": " << a[i] << " + " << b[i] << " = " << a[i] + b[i] << endl;
+ }
+ 
+ 
+ return 0;   
+}
