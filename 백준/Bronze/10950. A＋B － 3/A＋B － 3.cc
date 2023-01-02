140810cacb6e4c2373699847567fcd2542872d85
Hayley Son <hayleyson92@gmail.com>
2023-01-02 21:26:34 +0900

    Updated code for 10950

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -1,14 +1,33 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    
-    int t, a, b;
+// int main(){
+
+//     int t, a, b;
+//     cin >> t;
+
+//     for (int i = 0; i < t; i++){
+//         cin >> a >> b;
+//         cout << a + b << endl;
+//     }
+
+// }
+
+int main()
+{
+
+    int t;
     cin >> t;
-    
-    for (int i = 0; i < t; i++){
-        cin >> a >> b;
-        cout << a + b << endl;
+    int a[t];
+    int b[t];
+
+    for (int i = 0; i < t; i++)
+    {
+        cin >> a[i] >> b[i];
     }
 
+    for (int i = 0; i < t; i++)
+    {
+        cout << a[i] + b[i] << endl;
+    }
 }
